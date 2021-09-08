94ff9ff2795ae89ff2780e82ad2739106a10e4e8
Trestan Pillonel <trestan.pillonel@gmail.com>
2021-09-08 19:00:05 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='assembly_finder',
+      version=__version__,
+      packages=find_packages(),
+      package_data={"":["GENCOV/*", ]},
+      description='SARS-CoV-2 snp-calling and consensus generation - amplicon - Illumina paired-ends reads',
+      url='https://github.com/metagenlab/GENCOV',
+      author='Farid Chaabane & Trestan Pillonel',
+      author_email='trestan.pillonel@gmail.com',
+      entry_points="""
+      [console_scripts]
+      gencov = GENCOV.gencov:cli
+      """,
+      include_package_data=True,
+      keywords=[],
+      zip_safe=False,
+      install_requires=['python>=3.6.0', 
+                      'snakemake>=5.2', 
+                      'biopython>=1.77', 
+                      'strictyaml',
+                      'pyvcf', 
+                      'biopython'],
+    )
+
+
